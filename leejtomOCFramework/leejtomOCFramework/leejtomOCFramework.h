c47c92824951ca4128ea3e6e44ff19775815381b
lijingtong <lijingtong@polyv.net>
2021-04-13 17:22:48 +0800

    add

---
+++ leejtomOCFramework/leejtomOCFramework/leejtomOCFramework.h
@@ -0,0 +1,18 @@
+//
+//  leejtomOCFramework.h
+//  leejtomOCFramework
+//
+//  Created by mac on 2021/3/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for leejtomOCFramework.
+FOUNDATION_EXPORT double leejtomOCFrameworkVersionNumber;
+
+//! Project version string for leejtomOCFramework.
+FOUNDATION_EXPORT const unsigned char leejtomOCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <leejtomOCFramework/PublicHeader.h>
+
+
